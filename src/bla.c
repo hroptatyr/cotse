c3116cf55c97c9d607ee96a8da50b2f025e19a15
Sebastian Freundt <freundt@ga-group.nl>
2016-01-10 12:03:18 +0000

    chore, turn bla tool into front-end for cots_push()

--- src/bla.c
+++ src/bla.c
@@ -20,12 +20,6 @@
 #define strtopx		strtod32
 #define strtoqx		strtod64
 
-static cots_to_t them[16384U];
-static cots_mtrc_t mtrs[16384U];
-static cots_px_t bids[16384U];
-static cots_qx_t qtys[16384U];
-static size_t nthem;
-static unsigned char data[sizeof(them)];
 static cots_ob_t obar;
 
 static void
@@ -49,8 +43,6 @@ push(const char *line, size_t UNUSED(llen))
 	s = (s * NSECS + x);
 	//s /= MSECQ;
 	//s *= MSECQ;
-	them[nthem] = s;
-
 
 	with (const char *ecn = ++on) {
 		if (UNLIKELY((on = strchr(ecn, '\t')) == NULL)) {
@@ -59,102 +51,18 @@ push(const char *line, size_t UNUSED(llen))
 			/* fuck */
 			return;
 		}
-		mtrs[nthem] = m;
 	}
 
-	if (*++on != '\t' && (b = strtopx(on, &on))) {
-		bids[nthem] = b;
+	if (*++on == '\t' || isnand32(b = strtopx(on, &on))) {
+		return;
 	}
 
-	if (*++on != '\t' && (q = strtoqx(on, &on))) {
-		qtys[nthem] = q;
+	if (*++on == '\t' || isnand64(q = strtoqx(on, &on))) {
+		return;
 	}
-	nthem++;
+
+	cots_push(NULL, m, s, b, q);
 	return;
-}
-
-static void
-pr(void)
-{
-#if 1
-	cots_to_t last = 0U;
-
-	for (size_t i = 0U; i < 4U; i++) {
-		fprintf(stderr, "<- %+ld  <- %lu\n", them[i], them[i] - last);
-		last = them[i];
-	}
-	for (size_t i = 124U; i < 132U; i++) {
-		fprintf(stderr, "<- %+ld  <- %lu\n", them[i], them[i] - last);
-		last = them[i];
-	}
-	for (size_t i = 8188U; i < 8192U; i++) {
-		fprintf(stderr, "<- %+ld  <- %lu\n", them[i], them[i] - last);
-		last = them[i];
-	}
-#endif
-	return;
-}
-
-static void
-prpx(void)
-{
-#if 1
-	char sp[32U];
-	for (size_t i = 0U; i < 4U; i++) {
-		d32tostr(sp, sizeof(sp), bids[i]);
-		fputs(sp, stderr);
-		fputc('\n', stderr);
-	}
-	for (size_t i = 124U; i < 132U; i++) {
-		d32tostr(sp, sizeof(sp), bids[i]);
-		fputs(sp, stderr);
-		fputc('\n', stderr);
-	}
-	for (size_t i = 8188U; i < 8192U; i++) {
-		d32tostr(sp, sizeof(sp), bids[i]);
-		fputs(sp, stderr);
-		fputc('\n', stderr);
-	}
-#endif
-}
-
-static void
-prqx(void)
-{
-#if 1
-	char sp[64U];
-	for (size_t i = 0U; i < 4U; i++) {
-		d64tostr(sp, sizeof(sp), qtys[i]);
-		fputs(sp, stderr);
-		fputc('\n', stderr);
-	}
-	for (size_t i = 124U; i < 164U; i++) {
-		d64tostr(sp, sizeof(sp), qtys[i]);
-		fputs(sp, stderr);
-		fputc('\n', stderr);
-	}
-	for (size_t i = 8188U; i < 8192U; i++) {
-		d64tostr(sp, sizeof(sp), qtys[i]);
-		fputs(sp, stderr);
-		fputc('\n', stderr);
-	}
-#endif
-}
-
-static void
-prhx(cots_ob_t ob)
-{
-#if 1
-	for (size_t i = 0U; i < 4U; i++) {
-		fprintf(stderr, "%lx\t%s\n", mtrs[i], cots_mtrc_name(ob, mtrs[i]));
-	}
-	for (size_t i = 124U; i < 164U; i++) {
-		fprintf(stderr, "%lx\t%s\n", mtrs[i], cots_mtrc_name(ob, mtrs[i]));
-	}
-	for (size_t i = 8188U; i < 8192U; i++) {
-		fprintf(stderr, "%lx\t%s\n", mtrs[i], cots_mtrc_name(ob, mtrs[i]));
-	}
-#endif
 }
 
 int
@@ -164,80 +72,8 @@ main(int argc, char *argv[])
 	size_t llen = 0U;
 
 	obar = make_cots_ob();
-	memset(data, 0xef, sizeof(data));
 	for (ssize_t nrd; (nrd = getline(&line, &llen, stdin)) > 0;) {
 		push(line, nrd);
-
-		if (nthem >= countof(them)) {
-#if 1
-			size_t z = comp_to(data, them, countof(them));
-			size_t n;
-
-			fprintf(stderr, "%zu -> %zu\n", sizeof(them), z);
-			pr();
-
-			memset(them, 0, sizeof(them));
-
-			/* decode */
-			n = dcmp_to(them, data, z);
-			fprintf(stderr, "%zu -> %zu\n", z, n * sizeof(*them));
-
-			pr();
-
-#elif 0
-			size_t z = comp_px(data, bids, countof(bids));
-			size_t n;
-
-			fprintf(stderr, "%zu -> %zu\n", sizeof(bids), z);
-			prpx();
-
-			memset(bids, 0, sizeof(bids));
-
-			/* decode */
-			n = dcmp_px(bids, data, z);
-			fprintf(stderr, "%zu -> %zu\n", z, n * sizeof(*bids));
-
-			prpx();
-
-#elif 0
-			size_t z = comp_qx(data, qtys, countof(qtys));
-			size_t n;
-
-			fprintf(stderr, "%zu -> %zu\n", sizeof(qtys), z);
-			prqx();
-
-			memset(qtys, 0, sizeof(qtys));
-
-			/* decode */
-			n = dcmp_qx(qtys, data, z);
-			fprintf(stderr, "%zu -> %zu\n", z, n * sizeof(*qtys));
-
-			prqx();
-
-#elif 1
-			size_t z, oz;
-			size_t n;
-
-			oz = z = comp_ob(data, obar);
-			z += comp_mtrc(data + oz, mtrs, countof(mtrs));
-
-			fprintf(stderr, "%zu -> %zu\n", sizeof(mtrs), z);
-			prhx(obar);
-
-			memset(mtrs, 0, sizeof(mtrs));
-
-			/* decode */
-			cots_ob_t yay = dcmp_ob(data, oz);
-			n = dcmp_mtrc(mtrs, data + oz, z - oz);
-			fprintf(stderr, "%zu -> %zu\n", z, n * sizeof(*mtrs));
-
-			prhx(yay);
-
-#endif
-			write(STDOUT_FILENO, data, z);
-			nthem = 0U;
-			break;
-		}
 	}
 	close(STDOUT_FILENO);
 	free(line);
