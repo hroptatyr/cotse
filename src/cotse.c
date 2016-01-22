87ba549e22e9a66df0b4f3d8dc484c0731e71af6
Sebastian Freundt <freundt@ga-group.nl>
2016-01-22 09:05:22 +0000

    minor, make cots_freeze() idempotent

--- src/cotse.c
+++ src/cotse.c
@@ -109,6 +109,11 @@ struct _ss_s {
 	/* compacted version of fields */
 	char *fields;
 
+	/* flags for bookkeeping and stuff */
+	struct {
+		unsigned int frozen:1;
+	};
+
 	/* row-oriented page buffer */
 	struct pbuf_s pb;
 
@@ -801,10 +806,14 @@ cots_freeze(cots_ss_t s)
 	if (UNLIKELY(_s->fd < 0)) {
 		/* not on my watch */
 		return -1;
+	} else if (_s->frozen) {
+		return 0;
 	}
 
 	/* flush wal to file */
 	rc = _flush(_s);
+	/* set frozen flag */
+	_s->frozen = 1U;
 
 	if (_s->idx) {
 		/* this is bad coupling:
